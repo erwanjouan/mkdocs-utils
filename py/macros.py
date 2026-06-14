2b49caaa20182ce7384cbd8ba00257d3912576c9
Erwan Jouan <erwan.jouan@theatomicity.com>
2026-06-14 09:36:25 +0200

    update

--- py/macros.py
+++ py/macros.py
@@ -41,7 +41,14 @@ def define_env(env):
             "document.getElementById('secret-btn-{uid}').disabled = true;"
             "}})"
         ).format(cat=input_category, key=input_key, prop=input_property, uid=uid)
+        icon = (
+            '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"'
+            ' fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">'
+            '<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/>'
+            '<circle cx="12" cy="12" r="3"/>'
+            '</svg>'
+        )
         return (
             '<span id="secret-label-{uid}">******</span>'
-            ' <button id="secret-btn-{uid}" onclick="{js}">&#11015;</button>'
-        ).format(uid=uid, js=js)
+            ' <button id="secret-btn-{uid}" onclick="{js}">{icon}</button>'
+        ).format(uid=uid, js=js, icon=icon)
